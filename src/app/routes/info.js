3cb60cc689f2d5e39e83dd9ddfceedc6dc1f6209
callumc7 <callumc7@users.noreply.github.com>
2018-12-17 16:38:13 +0000

    Refactor

--- src/app/routes/info.js
+++ src/app/routes/info.js
@@ -1,12 +1,10 @@
 'use strict'
 
-module.exports = {
-  index: async(ctx) => {
-    ctx.status = 200
-    ctx.body = {
-      status: 'success',
-      data: null,
-      message: 'Countdown API - Check the docs for info on how to use'
-    }
-  }
+module.exports.index = async(ctx) => {
+  ctx.status = 200;
+  ctx.body = {
+    status: 'success',
+    data: null,
+    message: 'Countdown API - Check the docs for info on how to use'
+  };
 };
